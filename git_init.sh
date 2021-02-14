508543bc08fe8e26725fa76687611ed7ae5d2f44
胡飞飞 <hu.feifei@darcytech.com>
2021-02-14 16:54:22 +0800

    2021年 2月14日 星期日 16时54分22秒 CST

--- git_init.sh
+++ git_init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git remote add github.com  https://github.com/holmofy/www.hufeifei.cn
-git remote add www.hufeifei.cn hff@www.hufeifei.cn:~/.www.hufeifei.cn.git
+git remote add www.hufeifei.cn hff@www.hufeifei.cn:~/.repo/www.hufeifei.cn.git
