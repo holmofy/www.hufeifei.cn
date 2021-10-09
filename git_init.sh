aea84a8f700370aa4e5d61d5975baf4c592dc6bd
holmofy <hff1996723@163.com>
2021-10-09 22:00:32 +0800

    recommit

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git remote add github.com  https://github.com/holmofy/www.hufeifei.cn
+git remote add www.hufeifei.cn hff@www.hufeifei.cn:~/.repo/www.hufeifei.cn.git
---
+++ git_push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git add *
+current_date=$(date)
+git commit -m "${current_date}"
+git push github.com master
+git push www.hufeifei.cn master
+
